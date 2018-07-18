afe1fc9fc5e697d6827a5b212dcd57736580497d
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-18 20:50:12 +0000

    added /contact, /events, /teachers, and /about to mon..jascontroller

--- app/controllers/monks_and_ninjas_controller.rb
+++ app/controllers/monks_and_ninjas_controller.rb
@@ -6,4 +6,21 @@ class MonksAndNinjasController < ApplicationController #inherit from
     erb :home
   end
 
+  get '/about' do
+    erb :'monksandninjas/about'
+  end
+
+  get '/teachers' do
+    erb :'monksandninjas/teachers'
+  end
+
+  get '/contact' do
+    erb :'monksandninjas/contact'
+  end
+
+  get '/events' do
+    erb :'monksandninjas/events'
+  end
+
+
 end
