0c808502b4a9e2244c3987970f9437c756e94de3
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-18 21:00:14 +0000

    added post '/signup' to registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,4 +6,17 @@ class RegistrationsController < ApplicationController
     erb :'registrations/signup'
   end
 
+  post '/signup' do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      user = User.new(params)
+      user.save
+
+      session[:user_id] = user.id
+
+      redirect to '/about_me'
+    else
+      redirect to '/signup'
+    end
+  end
+
 end
