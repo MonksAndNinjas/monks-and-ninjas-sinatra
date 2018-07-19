4f5c8fbba4fa2f39d8d6bc3f50535d6d769030e8
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-19 20:03:55 +0000

    added '/posts/:id' route to posts controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ require './config/environment'
 
 class PostsController < ApplicationController
 
+  get '/posts/:id' do
+    if Helpers.is_logged_in?(session)
+      @post = Post.find_by_id(params[:id])
+
+      erb :'posts/show'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
