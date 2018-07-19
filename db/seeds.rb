1735c6594b1b7edce0f8ffe7206cedfde67047f5
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-19 00:36:35 +0000

    made changes to checkbox input in about_me page

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# place data for testing here!
+@fitness1 = FitnessModality.new(name: "Gymnastics")
+@fitness1.save
+@fitness2 = FitnessModality.new(name: "Martial Arts")
+@fitness2.save
+@fitness3 = FitnessModality.new(name: "Basketball")
+@fitness3.save
