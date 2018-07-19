063d52b7bd3178eda4c5f7b25bcafec8f3be3684
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-19 18:49:31 +0000

    added to '/users/:slug/edit' and created patch route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,25 @@ class UsersController < ApplicationController
   end
 
   patch '/users/:slug' do
-    
+    @user = User.find_by_slug(params[:slug])
+    @user.fitness_modalities.clear
+
+
+    @user.update(username: params[:username]) if !params[:username].empty?
+    @user.update(password: params[:password]) if !params[:password].empty?
+    @user.update(residence: params[:residence]) if !params[:residence].empty?
+    @user.update(fitness_level: params[:fitness_level]) if !params[:fitness_level].empty?
+    @user.fitness_modalities << FitnessModality.new(name: params[:modality_name]) if !params[:modality_name].empty?
+    @user.update(professional: params[:professional])
+
+    if params[:modalities].size > 0
+      params[:modalities].each do |modality_id|
+        @user.fitness_modalities << FitnessModality.find_by_id(modality_id)
+      end
+    end
+    @user.save
+
+    redirect to "/users/#{@user.slug}"
   end
 
   get '/logout' do
