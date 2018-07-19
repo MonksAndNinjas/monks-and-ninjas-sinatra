e7a7f653262bbb795506fe742837cd8eacf1fd3b
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-19 20:40:46 +0000

    moved methods in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ require './config/environment'
 
 class PostsController < ApplicationController
 
+  get '/posts/new' do
+    erb :'posts/new'
+  end
+
   get '/posts/:id' do
     if Helpers.is_logged_in?(session)
       @post = Post.find_by_id(params[:id])
@@ -15,17 +19,9 @@ class PostsController < ApplicationController
 
   get '/posts/:id/edit' do
     if Helpers.is_logged_in?(session)
+      @post = Post.find_by_id(params[:id])
 
       erb :'posts/edit'
-    else
-      redirect to '/login'
-    end
-  end
-
-  get '/posts/new' do
-    if Helpers.is_logged_in?(session)
-
-      erb :'posts/new'
     else
       redirect to '/login'
     end
