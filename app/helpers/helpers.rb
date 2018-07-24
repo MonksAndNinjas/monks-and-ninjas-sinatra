82faf9050649c47bbd2e94be13d2cc7f2d0d2c27
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-24 17:22:08 +0000

    added self.registered?(arg) to helpers

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -13,4 +13,19 @@ class Helpers
     @user
   end
 
+  def self.registered(arg)
+    @user = User.find(arg[:user_id])
+    if @user.residence == nil
+      false
+    elsif @user.professional == nil
+      false
+    elsif @user.fitness_level == nil
+      false
+    elsif @user.fitness_modalities.size == 0
+      false
+    else
+      true
+    end
+  end
+
 end
