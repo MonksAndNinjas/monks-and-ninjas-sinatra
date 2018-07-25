27c25048f55a6eb755eec08da3cad8eb2c139419
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-25 17:23:14 +0000

    removed include and extend from FitnessModality model

--- app/models/fitness_modality.rb
+++ app/models/fitness_modality.rb
@@ -1,7 +1,4 @@
 class FitnessModality < ActiveRecord::Base
   has_many :user_fitness_modalities
   has_many :users, through: :user_fitness_modalities
-
-  include Slugify::InstanceMethods
-  extend Slugify::ClassMethods
 end
