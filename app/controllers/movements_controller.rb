535223ed491e039d1aac0b7e5f1eb3cc65afb274
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-31 19:40:21 +0000

    created movements class in models folder

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -4,6 +4,17 @@ class MovementsController < ApplicationController
   use Rack::Flash
 
   get '/movements' do
-    erb :'movements/index'
+    if Helpers.is_logged_in?(session) && Helpers.registered?(session)
+      @user = User.find_by_id(session[:user_id])
+
+      erb :'movements/index'
+    elsif Helpers.is_logged_in?(session) && !Helpers.registered?(session)
+      flash[:message] = "Please complete registration"
+
+      redirect to '/about_me'
+    else
+      redirect to '/login'
+    end
+  end
   end
 end
---
+++ app/models/movement.rb
@@ -0,0 +1,3 @@
+class Movements < ActiveRecord::Base
+
+end
